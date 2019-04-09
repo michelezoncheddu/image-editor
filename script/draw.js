c21a3694164771493077cac6ef49cd771da34df9
michelezoncheddu <michelezoncheddu@users.noreply.github.com>
2019-04-09 21:39:13 +0200

    Centered rotate function and fixed pick correlation offset

--- script/draw.js
+++ script/draw.js
@@ -1,21 +1,18 @@
-"use strict";
+'use strict';
 
 function drawRotated(canvas, image, degrees) {
-	var context = canvas.getContext("2d");
+	var context = canvas.getContext('2d');
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	context.save();
-	context.translate(canvas.width / 2, canvas.height / 2);
+	context.translate(canvas.width / 2, canvas.height / 2); // rotate on center
 	context.rotate(degrees * Math.PI / 180);
 	context.translate(-canvas.width / 2, -canvas.height / 2);
-	
 	drawImage(canvas, image);
-
 	context.restore();
 }
 
 function drawImage(canvas, image) {
-	var context = canvas.getContext("2d");
-
+	var context = canvas.getContext('2d');
 	var ratio = image.width / image.height;
 	var width = canvas.width;
 	var height = width / ratio;
@@ -23,6 +20,5 @@ function drawImage(canvas, image) {
 		height = canvas.height;
 		width = height * ratio;
 	}
-
-	context.drawImage(image, 0, 0, width, height);
+	context.drawImage(image, (canvas.width - width) / 2, (canvas.height - height) / 2, width, height);
 }
--- script/script.js
+++ script/script.js
@@ -1,34 +1,39 @@
-"use strict";
+'use strict';
 
 $(function() {
-	var canvas = document.getElementById("editor");
+	var canvas = document.getElementById('editor');
 	canvas.height = window.innerHeight * 0.8;
 	canvas.width = window.innerWidth * 0.8;
-	$("#main").css("width", canvas.width);
-	var context = canvas.getContext("2d");
-	context.imageSmoothingQuality = "High";
-	context.mozImageSmoothingEnabled = false;
-	context.webkitImageSmoothingEnabled = false;
-	context.msImageSmoothingEnabled = false;
-	context.imageSmoothingEnabled = false;
+	$('#main').css('width', canvas.width);
+	var context = canvas.getContext('2d');
+	context.imageSmoothingQuality = 'High';
+	context.mozImageSmoothingEnabled = true;
+	context.webkitImageSmoothingEnabled = true;
+	context.msImageSmoothingEnabled = true;
+	context.imageSmoothingEnabled = true;
 
 	var angleInDegrees = 0;
 
-	var mouseClicked = false, mouseReleased = true;
-	document.addEventListener("click", onMouseClick, false);
-	document.addEventListener("mousemove", onMouseMove, false);
+	var mouseDown = false;
+	var mouseReleased = true;
+	document.addEventListener('mousedown', onMouseDown, false);
+	document.addEventListener('mousemove', onMouseMove, false);
 
-	function onMouseClick(e) {
-		mouseClicked = !mouseClicked;
+	function onMouseDown(evt) {
+		if (!mouseDown) {
+			var pos = getMousePos(canvas, evt)
+			if (pos.x >= 0 && pos.x <= canvas.width && pos.y >= 0 && pos.y <= canvas.height)
+				mouseDown = true;
+		}
 	}
 
-	function onMouseMove(e) {
+	function onMouseMove(evt) {
 		if (mouseClicked) {
+			var pos = getMousePos(canvas, evt)
 			context.beginPath();
-			console.log(e.clientX + " - " + e.clientY);
-			context.arc(e.clientX, e.clientY, 7.5, 0, Math.PI * 2, false);
+			context.arc(pos.x, pos.y, 7.5, 0, Math.PI * 2, false);
 			context.lineWidth = 5;
-			context.strokeStyle = "#fff";
+			context.strokeStyle = '#fff';
 			context.stroke();
 		}
 	}
@@ -37,15 +42,15 @@ $(function() {
 	image.onload = function() {
 		drawImage(canvas, image);
 	}
-	image.src = "img/test_2.jpg";
+	image.src = 'img/test_2.jpg';
 
-	$("#clockwise").click(function() {
-		angleInDegrees++;
+	$('#clockwise').click(function() {
+		angleInDegrees += 0.1;
 		drawRotated(canvas, image, angleInDegrees);
-	});
+	})
 	
-	$("#counterclockwise").click(function() {
-		angleInDegrees--;
+	$('#counterclockwise').click(function() {
+		angleInDegrees -= 0.1;
 		drawRotated(canvas, image, angleInDegrees);
-	});	
-});
+	})
+})
